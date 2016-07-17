476423b2408315c6e7c0a4366bce1cea5d4eec43
Cal Archer <carcher002@gmail.com>
2016-07-17 21:11:30 +0200

    Bluetooth: fix stuttering audio
    commit 2772d76990d8424ef3c629dba164fc7111b37db3 caused
    a regression in A2DP for Broadcom Bluetooth chipsets
    by not sending the HCI_BRCM_SET_ACL_PRIORITY command.
    
    Change this logic so the ACL priority adjustment occurs
    unconditionally for non-Broadcom chipsets only.
    
    Reproduction steps: Entering Bluetooth settings which
    triggers inquiry and makes the device discoverable while
    simultaneously using 2.4GHz WiFi.
    
    Change-Id: Ic40dc8d66ae30c71e0ddf1a2eaeb19fee1e87ecb

--- stack/l2cap/l2c_utils.c
+++ stack/l2cap/l2c_utils.c
@@ -2527,7 +2527,14 @@ BOOLEAN l2cu_set_acl_priority (BD_ADDR bd_addr, UINT8 priority, BOOLEAN reset_af
                 l2c_link_adjust_allocation();
             }
         }
+    } else {
+        if (p_lcb->acl_priority != priority)
+        {
+            p_lcb->acl_priority = priority;
+            l2c_link_adjust_allocation();
+        }
     }
+
     return(TRUE);
 }
 
